48fe680b7097500d12fd9cf0b1331b737b91e740
0x <joegikenye@gmail.com>
2025-05-03 00:32:41 +0300

    fix: Update type assertion for web3Context in Web3Provider

--- contexts/Web3Provider.tsx
+++ contexts/Web3Provider.tsx
@@ -6,7 +6,7 @@ import { Web3Context, useWeb3Provider, type Web3ContextType } from "./useWeb3";
 export function Web3Provider({ children }: { children: ReactNode }) {
   const web3Context = useWeb3Provider();
   return (
-    <Web3Context.Provider value={web3Context as Web3ContextType}>
+    <Web3Context.Provider value={web3Context as unknown as Web3ContextType}>
       {children}
     </Web3Context.Provider>
   );
